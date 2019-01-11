e2fed6e579d04de33632038b16b3c2177a178256
y <y@example.com>
2019-01-11 19:30:57 +0800

    brick0.1

--- Top.v
+++ Top.v
@@ -58,8 +58,8 @@ module Top(
 	
 	wire keyReady;
 	ps2tokey tokey(.clk(clk),.PS2_clk(PS2_clk),.PS2_data(PS2_data),.key_pressed(keyCodeBoard));
-	Keypad k0 (.clk(clkdiv[15]), .keyX(BTN_Y), .keyY(BTN_X),.keyCode(keyCodePad), .ready(keyReady));
-	assign keyCode = (keyCodeBoard == 0) ? keyCodeBoard : keyCodePad;
+	//Keypad k0 (.clk(clkdiv[15]), .keyX(BTN_Y), .keyY(BTN_X),.keyCode(keyCodePad), .ready(keyReady));
+	//assign keyCode = (keyCodeBoard == 0) ? keyCodeBoard : keyCodePad;
 	
 	wire [31:0] segTestData;
 	wire [3:0]sout;
@@ -73,8 +73,8 @@ module Top(
 	assign SEGLED_CLR = sout[0];
  	
 	
-	reg [9:0] x;
-	reg [8:0] y;
+	reg [3:0] x = 4'd4;
+	reg [3:0] y = 4'd8;
 	
  	reg [11:0] rgb_reg, rgb_next;
  	wire [9:0] col_addr;
@@ -94,6 +94,7 @@ module Top(
 		.r(r), .g(g), .b(b), .hs(hs), .vs(vs)
 	);
 	reg wasReady;
+
 	
     ///////////////////
 	// Breakout part //
@@ -113,6 +114,8 @@ module Top(
 	reg gra_still;
 	wire hit, miss;
 	
+	wire [3:0] dig0, dig1;
+	
 	assign video_on = 1'b1;
 	
    //=======================================================
@@ -120,75 +123,83 @@ module Top(
    //=======================================================
 	   // instantiate graph module
     pong_graph graph_unit
-        (.clk(clkdiv), .reset(!rstn), .btn(keyCode),
+        (.clk(clkdiv), .reset(!rstn), .btn(keyCodeBoard),
         .pix_x(col_addr), .pix_y(row_addr),
         .gra_still(SW[0]), .hit(hit), .miss(miss),
         .graph_on(graph_on), .graph_rgb(graph_rgb));
         
-   //=======================================================
-   // title screen
-   //=======================================================
-   /**/  pong_text text_unit(.clk(clk),
-       .pix_x(col_addr), .pix_y(row_addr),
-       .dig0(4'b1), .dig1(4'b1), .ball(2'b1),
-       .text_on(text_on), .text_rgb(text_rgb));
+//    pong_text text_unit
+//      (.clk(clk),
+//       .pix_x(col_addr), .pix_y(row_addr),
+//       .dig0(dig0), .dig1(dig1), .ball(ball_reg),
+//       .text_on(text_on), .text_rgb(text_rgb));
+	
 
    //=======================================================
    // finite state machine
    //=======================================================
 	always @(posedge clk, negedge rstn)
-       if (!rstn)
-          begin
-             state_reg <= newgame;
-             ball_reg <= 0;
-             rgb_reg <= 12'h000;
-          end
-       else
-          begin
-            state_reg <= state_next;
-            ball_reg <= ball_next;
+       if (!rstn) begin
+         state_reg <= newgame;
+         ball_reg <= 0;
+         rgb_reg <= 12'h000;
+      end
+       else begin
+         state_reg <= state_next;
+         ball_reg <= ball_next;
 //            if (pixel_tick)
-               rgb_reg <= rgb_next;
-          end
+         rgb_reg <= rgb_next;
+       end
 	
     always @*
       if (~video_on)
          rgb_next = "000"; // blank the edge/retrace
       else
          // display score, rule, or game over
-			if (text_on[2]&&SW[0]==1) // display logo
-           rgb_next = text_rgb;
-         else if (graph_on)  // display graph
+//         if (text_on[3] ||
+//               ((state_reg==newgame) && text_on[1]) || // rule
+//               ((state_reg==over) && text_on[0]))
+//            rgb_next = text_rgb;
+//         else 
+            if (graph_on)  // display graph
            rgb_next = graph_rgb;
-         else 
+//         else if (text_on[2]) // display logo
+//           rgb_next = text_rgb;
+         else
            rgb_next = 12'hff0; // yellow background
    
-   // output
-//    always @* begin
-//        vga_data <= {{4{rgb_reg[2]}}, {4{rgb_reg[1]}}, {4{rgb_reg[0]}}};
-//        vga_data <= rgb_reg;
-//    end
-	
-//	assign x_sqr = (x - col_addr) * (x - col_addr);
-//	assign y_sqr = (y - row_addr) * (y - row_addr);
-//	assign r_sqr = radius * radius;
-	
-//	always@(*) begin
-//		if ((x_sqr + y_sqr < r_sqr))
-//			vga_data <= SW[12:1];
-//		else vga_data <= 12'hfff;
-//	end
+
     wire [18:0] blk_mem_0_d;
-    
-    blk_mem_gen_0(.clka(clkdiv), .addra(blk_mem_0_d), .douta(ip_out));
+
+    //blk_mem_gen_0 v0(.clka(clkdiv), .addra(blk_mem_0_d), .douta(ip_out));
 
     assign blk_mem_0_d = row_addr * 640 + col_addr;
+    
+     reg [7:0] counter;
+     initial begin
+        counter = 8'd0;
+     end
+     always @(hit) begin
+       if(counter[3:0] < 4'd9)
+            counter[3:0] <= counter[3:0] + 4'd1;
+        else if(counter[3:0] == 4'd9)begin
+            counter[3:0] <= 4'd0;
+            if(counter[7:4] < 4'd9)
+                counter[7:4] <= counter[7:4] + 4'd1;
+            else
+                counter[7:4] <= 4'd0;
+        end
+     end
 
-	assign segTestData = {7'b0,x,8'b0,y};
+	assign segTestData = {24'd0, counter};
 	wire [15:0] ledData;
 	assign ledData = SW_OK;
+	
 	ShiftReg #(.WIDTH(16)) ledDevice (.clk(clkdiv[3]), 
 	.pdata(~ledData), .sout({LED_CLK,LED_DO,LED_PEN,LED_CLR}));
+    
+    // counter
 
 
 endmodule
+
